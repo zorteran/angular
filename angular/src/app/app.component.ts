f57e001614aa08a60485e798e8abacbca69c3567
Maciej <zorteran@gmail.com>
2018-10-22 13:54:08 +0200

    Added test list component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 
+class Item{
+  id:number;
+  name:string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'angular';
 
-  hide = false;
+  hide = true;
 
-  color = 'red';
+  color: 'red' | 'green' = 'red';
+
+  public items: Item[]= [
+    {id:1, name: 'Lublin'},
+    {id:2, name: 'Warszawa'},
+    {id:3, name: 'Gdańsk'},
+    {id:4, name: 'Rzeszów'},
+    {id:5, name: 'Suwałki'},
+  ];
 
   toggleHide() {
     this.hide = !this.hide;
@@ -22,6 +37,9 @@ export class AppComponent {
     } else {
       this.color = 'red';
     }
+  }
+  onItemClick(item){
+    console.log('ITEM CLICKED', item);
   }
 }
 
