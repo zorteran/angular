a2196548bbfc48a61df7ecb170b185fa6f40f224
Maciej <zorteran@gmail.com>
2018-10-22 17:09:01 +0200

    Added map based simple todolist with a from

--- angular/src/app/test/map/map.component.ts
+++ angular/src/app/test/map/map.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+
+export interface Coords {
+  lat: number;
+  lng: number;
+}
 
 @Component({
   selector: 'app-map',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  @ViewChild('mapContainer') mapContainer: ElementRef;
   constructor() { }
+  map: any = {};
+
+  @Output() mapClick = new EventEmitter<Coords>();
 
   ngOnInit() {
+    console.log(this.mapContainer.nativeElement, L);
+    this.initializeMap();
   }
+
+
+  initializeMap() {
+    this.map = L.map(this.mapContainer.nativeElement).setView([52.226488, 21.001659], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    this.map.on('click', this.onMapClick, this);
+  }
+
+  onMapClick(e) {
+    this.mapClick.next(e.latlng);
+  }
+
 
 }
--- angular/src/app/test/test.module.ts
+++ angular/src/app/test/test.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ListComponent } from './list/list.component';
 import { MapComponent } from './map/map.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [ListComponent, MapComponent],
   exports: [ListComponent]
