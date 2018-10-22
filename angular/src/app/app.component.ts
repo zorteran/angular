cb478065d24d27e8a9ce2a3f0ae19a99bc75c924
Maciej <zorteran@gmail.com>
2018-10-22 11:39:58 +0200

    Added toogle for hide and color

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular';
+
+  hide = false;
+
+  color = 'red';
+
+  toggleHide() {
+    this.hide = !this.hide;
+  }
+
+  toggleColor() {
+    if (this.color === 'red') {
+      this.color = 'green';
+    } else {
+      this.color = 'red';
+    }
+  }
 }
+
