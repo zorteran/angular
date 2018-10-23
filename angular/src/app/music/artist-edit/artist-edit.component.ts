2d049f26b79413aca59163117bfd23ff06b22a11
Maciej <zorteran@gmail.com>
2018-10-23 16:49:44 +0200

    Oh, another update artist subscribe thing

--- angular/src/app/music/artist-edit/artist-edit.component.ts
+++ angular/src/app/music/artist-edit/artist-edit.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MusicService } from '../music.service';
-import { switchMap, tap } from 'rxjs/operators';
+import { switchMap, tap, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-artist-edit',
   templateUrl: './artist-edit.component.html',
   styleUrls: ['./artist-edit.component.scss']
 })
-export class ArtistEditComponent implements OnInit {
+export class ArtistEditComponent implements OnInit, OnDestroy {
+
   constructor(
     private route: ActivatedRoute,
     private musicService: MusicService,
@@ -17,23 +19,27 @@ export class ArtistEditComponent implements OnInit {
   ) { }
   artist$;
   artistForm: FormGroup;
-
+  destroy$ = new Subject();
 
   ngOnInit() {
     this.artist$ = this.route.paramMap.pipe(
+      takeUntil(this.destroy$),
       switchMap((paramsMap) => {
         return this.musicService.getArtist(paramsMap.get('id'));
-      }),
-      tap((artist) => {
-        this.artistForm.patchValue(artist);
       })
-    );
+    ).subscribe((artist) => {
+      this.artistForm.patchValue(artist);
+    });
 
     this.artistForm = new FormGroup({
       id: new FormControl(''),
       name: new FormControl(''),
       img: new FormControl(''),
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(1);
   }
 
   public save() {
