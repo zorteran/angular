261b6ac48ac01d1e6c2fb98448a7f7c50af8f446
Maciej <zorteran@gmail.com>
2018-10-23 11:36:04 +0200

    Added ngIf else waiting

--- angular/src/app/music/search/search.component.ts
+++ angular/src/app/music/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MusicService } from '../music.service';
 import { Observable } from 'rxjs';
 import { Artist } from '../models/artist.model';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -11,14 +12,14 @@ import { Artist } from '../models/artist.model';
 export class SearchComponent implements OnInit {
 
 
-  public artists$: Observable<Artist[]>;  // $ na końcu - jeśli cos jest streamem 
-
-  public show = false;
+  public artists$: Observable<Artist[]>;  // $ na końcu - jeśli cos jest streamem
 
   constructor(private musicService: MusicService) { }
 
   ngOnInit() {
-    this.artists$ = this.musicService.getArtists();
+    this.artists$ = this.musicService.getArtists().pipe(
+      delay(2000)
+    );
   }
 
 }
