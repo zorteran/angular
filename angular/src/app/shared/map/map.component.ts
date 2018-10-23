17ff6dbfe75b770bd2e298d4f529d2a586e1d684
Maciej <zorteran@gmail.com>
2018-10-23 14:56:18 +0200

    map changes

--- angular/src/app/shared/map/map.component.ts
+++ angular/src/app/shared/map/map.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Artist } from 'src/app/music/models/artist.model';
+import { ReplaySubject } from 'rxjs';
 
 export interface Coords {
   lat: number;
@@ -11,10 +12,12 @@ export interface Coords {
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnChanges {
+
 
   @ViewChild('mapContainer') mapContainer: ElementRef;
   @Input() artist: Artist;
+  private artist$ = new ReplaySubject<Artist>(1);
   constructor() { }
   map: any = {};
 
@@ -23,8 +26,14 @@ export class MapComponent implements OnInit {
   ngOnInit() {
     console.log(this.mapContainer.nativeElement, L);
     this.initializeMap();
+
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.hasOwnProperty('artist')) {
+      this.artist$.next(changes['artist'].currentValue);
+    }
+  }
 
   initializeMap() {
     const lat = this.artist != null ? this.artist.location.lat : 51.505;
@@ -36,10 +45,13 @@ export class MapComponent implements OnInit {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-    if (this.artist != null) {
+    this.artist$.subscribe((artist: Artist) => {
       L.marker([lat, lng]).addTo(this.map)
-        .bindPopup(this.artist.name)
+        .bindPopup(artist.name)
         .openPopup();
+    })
+    if (this.artist != null) {
+
     }
 
     this.map.on('click', this.onMapClick, this);
