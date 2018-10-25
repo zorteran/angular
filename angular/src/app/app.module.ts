ce3e2bace8c182cc25d7ba82970239b9df623d13
Maciej <zorteran@gmail.com>
2018-10-25 12:29:03 +0200

    added ng material

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BASE_URL, IMAGE_BASE_URL } from './app-config';
 import { environment } from 'src/environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { environment } from 'src/environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [{
     provide: BASE_URL,
