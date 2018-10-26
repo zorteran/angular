c921863898a306c942a2d337f6c5e16c324543b4
Maciej <zorteran@gmail.com>
2018-10-26 09:14:38 +0200

    finished user guard

--- angular/src/app/user.guard.ts
+++ angular/src/app/user.guard.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from './user.service';
+import { take, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserGuard implements CanActivate {
+  constructor(private userService: UserService) {
+
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    return this.userService.user$.pipe(
+      take(1),
+      map(u => !!u)
+    );
   }
 }
